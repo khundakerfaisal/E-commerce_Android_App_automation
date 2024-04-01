6b960560da30200ea313dc182d043477acffe1fd
khundakerfaisal <khundakermis@gmail.com>
2024-04-01 17:01:57 +0600

    Home page sctoll initiation

--- src/test/java/homePageScreen.java
+++ src/test/java/homePageScreen.java
@@ -1,28 +1,43 @@
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
 public class homePageScreen {
-    @FindBy(id="com.nopstation.nopcommerce.nopstationcart:id/btnAccept")
+    @FindBy(id = "com.nopstation.nopcommerce.nopstationcart:id/btnAccept")
     WebElement btnAcceptAgreement;
 
     @FindBy(className = "android.widget.ImageView")
     List<WebElement> btnProductIcon;
 
-    @FindBy(id="com.nopstation.nopcommerce.nopstationcart:id/ivProductThumb")
-    WebElement btnProductSubItem;
+    @FindBy(id = "com.nopstation.nopcommerce.nopstationcart:id/ivProductThumb")
+    List<WebElement> btnProductSubItem;
 
 
-    public homePageScreen(AndroidDriver driver){
-        PageFactory.initElements(new AppiumFieldDecorator(driver),this);
+    public homePageScreen(AndroidDriver driver) {
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
-public void selectProduct(){
-    btnAcceptAgreement.click();
-    btnProductIcon.get(1).click();
+
+    public void selectProduct() throws InterruptedException {
+        btnAcceptAgreement.click();
+        Thread.sleep(5000);
+        btnProductIcon.get(1).click();
+
+        btnProductSubItem.click();
+
+        Thread.sleep(3000);
+
+
+
     }
 
 }
--- src/test/java/homePageTestRunner.java
+++ src/test/java/homePageTestRunner.java
@@ -2,7 +2,7 @@ import org.testng.annotations.Test;
 
 public class homePageTestRunner extends setup{
     @Test(priority = 1,description = "Select Product")
-    public void searchProduct(){
+    public void searchProduct() throws InterruptedException {
         homePageScreen homePageScreen=new homePageScreen(driver);
         homePageScreen.selectProduct();
     }
